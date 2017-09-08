5c21741b02b5fed4d1eb2a610afed891557e229a
Ghabriel <ghabriel.nunes@gmail.com>
2017-09-08 19:06:12 -0300

    Minor improvements to promise-related functions

--- scripts/backend/UserPersistence.ts
+++ scripts/backend/UserPersistence.ts
@@ -30,72 +30,65 @@ function getConnection(): Promise<MySqlConnection> {
     });
 }
 
+function runQuery(connection: MySqlConnection, query: string,
+    params?: string[]): Promise<[any, any]> {
+
+    return new Promise(function(resolve, reject) {
+        connection.query(query, params, function(error, results, fields) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve([results, fields]);
+            }
+        });
+
+        connection.end();
+    });
+}
+
 export namespace UserPersistence {
     export function verify(username: string, password: string): Promise<boolean> {
-        return new Promise(function(resolve, reject) {
-            getConnection().then(function(connection) {
-                let query = "SELECT * FROM `users` WHERE `username` = ? AND `password` = ?";
-                connection.query(query, [username, password],
-                    function(error, results, fields) {
-
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(results.length != 0);
-                    }
-                });
-            });
+        return getConnection().then(function(connection) {
+            let query = "SELECT * FROM `users` WHERE `username` = ? AND `password` = ?";
+            return runQuery(connection, query, [username, password]);
+        }).then(function(data) {
+            let [results, fields] = data;
+            return (results.length != 0);
         });
     }
 
     export function register(user: User): Promise<boolean> {
-        return new Promise(function(resolve, reject) {
-            findUser(user).then(function(found) {
-                if (found) {
-                    resolve(false);
-                    return;
-                }
+        let nickname = user.getNickname();
+        let fullName = user.getFullName();
+        let email = user.getEmail();
+        let password = user.getPassword();
 
-                getConnection().then(function(connection) {
-                    let nickname = user.getNickname();
-                    let fullName = user.getFullName();
-                    let email = user.getEmail();
-                    let password = user.getPassword();
+        let query = "INSERT INTO users(username, nickname, fullName, email, password)\
+                     VALUES('" + nickname +"','" + nickname + "','" + fullName + "',\
+                     '"+ email +"','" + password +"')";
 
-                    let query = "INSERT INTO users(username, nickname, fullName, email, password)\
-                                 VALUES('" + nickname +"','" + nickname + "','" + fullName + "',\
-                                 '"+ email +"','" + password +"')";
+        return findUser(user).then(function(found) {
+            if (found) {
+                return false;
+            }
 
-                    connection.query(query, function(error) {
-                        if (error) {
-                            reject(error);
-                        } else {
-                            console.log(query);
-                            resolve(true);
-                        }
-                    });
-
-                    connection.end();
-                });
+            return getConnection().then(function(connection) {
+                return runQuery(connection, query);
+            }).then(function(data) {
+                console.log(query);
+                return true;
             });
         });
     }
 
     function findUser(user: User): Promise<boolean> {
-        return new Promise(function(resolve, reject) {
-            getConnection().then(function(connection) {
-                let query = "SELECT * FROM `users` WHERE `username` = ?";
-                connection.query({sql: query, values: [user.getNickname()]},
-                    function(error, results, fields) {
-
-                    if(error) {
-                        reject(error);
-                    } else {
-                        console.log(results);
-                        resolve(results.length != 0);
-                    }
-                });
-            });
+        return getConnection().then(function(connection) {
+            let query = "SELECT * FROM `users` WHERE `username` = ?";
+            return runQuery(connection, query, [user.getNickname()]);
+        }).then(function(data) {
+            let [results, fields] = data;
+            console.log(results);
+            return (results.length != 0);
         });
     }
 }
