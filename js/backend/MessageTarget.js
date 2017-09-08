686c006510cf4d427552286ce48d806ef0a3f7c7
Ghabriel <ghabriel.nunes@gmail.com>
2017-09-08 14:53:39 -0300

    Removed automatic files from git

--- js/backend/MessageTarget.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var MessageTarget;
-(function (MessageTarget) {
-    MessageTarget[MessageTarget["SENDER"] = 0] = "SENDER";
-    MessageTarget[MessageTarget["OTHERS"] = 1] = "OTHERS";
-    MessageTarget[MessageTarget["EVERYONE"] = 2] = "EVERYONE";
-})(MessageTarget = exports.MessageTarget || (exports.MessageTarget = {}));
--- js/backend/PackageIndex.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var std_1 = require("./packages/std");
-var chess_1 = require("./packages/chess");
-exports.packageIndex = {
-    "std": std_1.std,
-    "chess": chess_1.chess
-};
